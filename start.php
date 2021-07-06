f8f4607430147b55c02392502245e1fa7b9e6183
wowTerrific <61386423+wowTerrific@users.noreply.github.com>
2021-07-06 09:59:23 -0400

    Update start.php
    Development at src/pages/

--- start.php
+++ start.php
@@ -1,7 +1,8 @@
 <?php
 
-//start dev server for public/
+// start dev server for public/
+// shell_exec("php -S localhost:8000 -t ./public/");
 
 
-shell_exec("cd public/");
-shell_exec("php -S localhost:8000 -t public/");
+// start dev server for src/ 
+shell_exec("php -S localhost:3000 -t ./src/pages/");
