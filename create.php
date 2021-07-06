793b49dd53311a51cb84ab8138c97828fd29cf36
wowTerrific <61386423+wowTerrific@users.noreply.github.com>
2021-07-06 06:07:00 -0400

    Update create.php
    Test GLOB_ONLYDIR

--- create.php
+++ create.php
@@ -1,5 +1,13 @@
 <?php
 
+$public = "./public/";
+$pages = "./src/pages";
+
 // loop through src/pages 
+
+foreach( glob( $pages . "*.PHP", GLOB_ONLYDIR) as $filename ) {
+  echo $filename . "\n";
+}
+
 // create/open file if it exists and write/update
 
