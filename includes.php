cac6939adc1716f5d27523836c365df10dfda651
wowTerrific <61386423+wowTerrific@users.noreply.github.com>
2021-07-07 18:24:59 -0400

    Create includes.php
    New file to be included for every page. Hope to use this to solve for standard paths

---
+++ includes.php
@@ -0,0 +1,9 @@
+<?php
+
+// Paths
+
+$_ROOT = shell_exec('pwd');
+$_PAGES = $_ROOT . 'src/pages/';
+$_COMPONENTS = $_ROOT . 'src/components/';
+$_NSTAT = $_ROOT . 'src/non-static';
+$_PUBLIC = $_ROOT . 'public/';
